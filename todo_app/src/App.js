ae88f9e082822c14ece24e1a6fe5cf4a48b69265
sead-afk <61579298+sead-afk@users.noreply.github.com>
2022-01-15 23:20:56 +0100

    1st 30 min

---
+++ todo_app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  const sum = 1 + 1;
+  return (
+    <div className="App">
+      <h1>Hello Clever Programmers {sum}</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Tkae dogs for a walk</li>
+        <li>Tkae the rubbish out</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
