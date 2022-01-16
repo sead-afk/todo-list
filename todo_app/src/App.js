29ec6da9533cc1bd1a43ddbdcf2479cfd50edaf1
NerminKapetanovic <61951092+NerminKapetanovic@users.noreply.github.com>
2022-01-16 21:31:27 +0100

    Update App.js
    2/6 Nermin K

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,19 +1,38 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const sum = 1 + 1;
-  return (
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take the rubbish out']) ;
+  const [input, setInput] = useState('');
+  console.log('emoji', input);
+ 
+   const addTodo = (Event) => {
+// this will fire off when we click the button
+Event.preventDefault(); // Stops the refresh
+console.log('drugi emoji', 'I am working !!!')
+setTodos([...todos, input]);
+setInput(); //clearing input after submitting (clicking todo button)
+}
+  
+return (
     <div className="App">
-      <h1>Hello Clever Programmers {sum}</h1>
-      <input />
-      <button>Add Todo</button>
-
+      <h1>Hello Clever Programmers </h1>
+      <form>
+       <input value={input} onChange={Event => setInput(Event.target.value) }/>
+       <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
+      
       <ul>
-        <li>Tkae dogs for a walk</li>
-        <li>Tkae the rubbish out</li>
+        {todos.map(todo => (
+         <li>{todo}</li> 
+        ))}
         </ul>
     </div>
   );
 }
 
 export default App;
+
+
+
+
